33f4e8720472ed90084ec12ab001626a36bcdfe4
clpowe <33496540+clpowe@users.noreply.github.com>
2023-03-26 11:58:54 -0400

    Lesson 15

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config'
 import image from '@astrojs/image'
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://astro-blog.netlify.app/',
 	integrations: [
+		sitemap(),
 		image({
 			serviceEntryPoint: '@astrojs/image/sharp'
 		})
